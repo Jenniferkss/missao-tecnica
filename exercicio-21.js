aec51047ba3a1a58be8b1e236e812df525199c4c
Jennifer <jennifer.k.silva7@aluno.senai.br>
2025-06-06 11:25:44 -0300

    feat: Professor pediu para pular a questão

---
+++ exercicio-21.js
@@ -0,0 +1,28 @@
+/*Filtre da Beta apenas os itens que custam mais de 7000 créditos.*/ 
+let inventarioBeta = [
+    
+    [ 'Lâmina Solar', 'Mercúrio', 6700 ],
+    [ 'Cometa Engarrafado', 'Netuno', 7600 ],
+    [ 'Relíquia de Zordon', 'Elysium', 25000 ],
+    [ 'Nevoeiro Encapsulado', 'Júpiter', 3800 ],
+    [ 'Chip de Matéria', 'Ceres', 4100 ],
+    [ 'Chave de Portais', 'Júpiter', 5900 ],
+    [ 'Globo de Plasma', 'Terra', 7200 ],
+    [ 'Mapa Estelar', 'Vênus', 2100 ],
+    [ 'Pedra Temporal', 'Lua', 7800 ],
+    [ 'Lanterna Estelar', 'Sol', 3200 ],
+    [ 'Fragmento de Eclipse', 'Eclipse E-9', 8300 ],
+    [ 'Ponte de Gelo', 'Hoth', 3900 ],
+    [ 'Fluido de Saturno', 'Saturno', 4400 ],
+    [ 'Memória Galáctica', 'Terra', 5500 ],
+    [ 'Livro de Nebulosa', 'Andrômeda', 9200 ],
+    [ 'Túnel Quântico', 'Netuno', 4100 ],
+    [ 'Canhão de Luz', 'Vênus', 6800 ],
+    [ 'Orbe Incandescente', 'Sol', 9300 ],
+    [ 'Matriz de Propulsão', 'Vênus', 4500 ],
+    [ 'Partícula Livre', 'Urano', 5400 ],
+    [ 'Capa de Invisibilidade', 'Terra', 3900 ],
+    [ 'Mapa neurotico', 'Kepler', 3600 ],
+    [ 'Glopo estelar', 'Kepler', 2000 ]
+   ];
+let inventarioFiltrado = inventarioBeta.filter(inventario => inventarioBeta.)
